7f26e6df725f1de04c64f39299393300f265b2a6
Tomasz Kunicki <tomasz.kunicki@cherrygroup.com>
2014-12-24 12:35:51 +0100

    Change include to work after composer load to vendor

--- src/Behat/Context/Assert/include.php
+++ src/Behat/Context/Assert/include.php
@@ -2,5 +2,5 @@
 /**
  * User: Tomasz Kunicki
  */
-require_once __DIR__.'/../../../../vendor/autoload.php';
-require_once __DIR__.'/../../../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+require_once __DIR__.'./../../../../../../autoload.php';
+require_once __DIR__.'./../../../../../../phpunit/phpunit/src/Framework/Assert/Functions.php';
